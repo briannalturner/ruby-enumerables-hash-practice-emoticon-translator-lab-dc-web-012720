db5d57e2fc29d1d872fddfc30c3bf666eeeea48f
Bri Turner <blturner3527@gmail.com>
2020-01-15 03:58:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,21 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  file = YAML.load_file(file_path)
+  # p file
+  hash = {
+    :get_meaning => {},
+    :get_emoticon => {}
+  }
+  file.each { |key, value|
+    hash[:get_meaning][value[1]] = key
+    hash[:get_emoticon][value[0]] = value[1]
+  }
+  pp hash
+  hash
 end
+
+load_library('lib/emoticons.yml')
 
 def get_japanese_emoticon
   # code goes here
